bf647d2f5dcbb0a7cab87e194eed0a7ab23c87f4
Sanaz M <sanazmohammaddi@gmail.com>
2022-04-12 20:03:39 +0100

    Add to Cart method

--- first-app/main.js
+++ first-app/main.js
@@ -1,7 +1,20 @@
 Vue.createApp({
   data() {
     return {
-      product: 'Socks'
+      cart: 0,
+      product: 'Socks',
+      image: './assets/gray.jpg',
+      inventory: 5,
+      details: ['50% cotton', '30% wool', '20% polyster'],
+      variants: [
+        {id: 223, color: 'gray'},
+        {id: 224, color: 'purple'}
+      ]
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
     }
   }
 }).mount('#app')
